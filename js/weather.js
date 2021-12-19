00c2b8ee9387ec739189c2e5fb31609ddc00d130
Boyoon Choi <choiboyoon11@gmail.com>
2021-12-19 23:25:07 +0100

    add weather function

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+const API_KEY = "c3d928645f323fba6bf6b39b557baccf";
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:last-child");
+      const weather = document.querySelector("#weather span:first-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("error! can't find you. No weather for you ;(");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
+
+//my latitude : 2.3316439
+//my longitude : 48.809587
