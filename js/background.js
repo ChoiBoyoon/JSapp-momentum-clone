0719c2234679450d97525b2806ef6e2fc0e1031a
Boyoon Choi <choiboyoon11@gmail.com>
2022-01-09 23:52:59 +0100

    change background image according to current hour

--- js/background.js
+++ js/background.js
@@ -28,9 +28,29 @@ const images = [
   "26.jpeg",
 ];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const b_hours = new Date().getHours();
+console.log(b_hours);
+const chosenImgae = "";
+if (b_hours >= 4 && b_hours < 12) {
+  // 오전 4시 - 정오 사이(아침)
+  chosenImage = images[getRandomInt(0, 5)];
+} else if (b_hours >= 12 && b_hours < 17) {
+  //정오 - 오후 5시 사이(낮)
+  chosenImage = images[getRandomInt(6, 10)];
+} else if (b_hours >= 17 && b_hours < 20) {
+  //오후 5시 - 오후 8시 사이(저녁)
+  chosenImage = images[getRandomInt(11, 15)];
+} else {
+  //그 외(오후8시-오전4시. 밤)
+  chosenImage = images[getRandomInt(16, 26)];
+}
 
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
-
 document.body.appendChild(bgImage);
